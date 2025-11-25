7812a13738c673fcac422a2be0d2bb4355e290c0
Ren <neraj.mendoza@yahoo.com>
2025-11-25 13:47:42 +0800

    feat(ui): login and dashboard page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { LoginForm } from "@/components/login-form"
 
-export default function Login() {
-    return (
-        <div>Login <Button>Click me</Button></div>
-    );
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
